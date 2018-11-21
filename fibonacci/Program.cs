d8f7adacd5d1e4ea1c518aea3faf4e13d2ce0b3e
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-21 12:54:21 +1100

    added README.md and implemented fibonacci console app

---
+++ fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getFibonacci(21);
+        }
+
+        public static void getFibonacci(int elements){
+            var fibonacci = new List<int> {1, 1};
+            while (fibonacci.Count<elements)
+            {
+                var previous = fibonacci[fibonacci.Count - 1];
+                var previous2 = fibonacci[fibonacci.Count - 2];
+                fibonacci.Add(previous + previous2);
+            }        
+
+            int counter = 1;
+            foreach(var item in fibonacci){
+                Console.WriteLine($"Fibonacci({counter}): {item}");
+                counter++;
+            }
+        }
+
+
+    }
+}
